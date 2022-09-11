7dee22b946bbf3564d9fef610444a0f680f2a092
Tymur-Popov <85408680+Tymur-Popov@users.noreply.github.com>
2022-09-11 13:11:30 +0000

    Deploying to gh-pages from @ Tymur-Popov/goit-react-hw-05-movies@13bfe0a8039ad710edb73f249fbf341991b5424d 🚀

---
+++ static/js/805.80dbab82.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,r,t){t.r(r);var n=t(501),o=t(184);r.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h2",{children:"Sorry..Nothing found:("}),(0,o.jsx)(n.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=805.80dbab82.chunk.js.map
